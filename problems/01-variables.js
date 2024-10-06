d203b55a8fe7e3a694843ddf5db68f5bcf9a3ee6
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-06 18:22:23 -0500

    problem 1 complete

--- problems/01-variables.js
+++ problems/01-variables.js
@@ -7,35 +7,36 @@ test your work by running your code and the test specs!
 //    as a string:
 
 // Your code here 
-
+let firstName = "Raul";
 
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
 // Your code here 
-
+let lastName = " Saenz ";
 
 // 3. Declare a variable called `age`:
 
 // Your code here 
-
+let age;
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
 
+// Your code here 
+console.log(firstName + lastName)
 
 // 5. Assign the `age` variable to a number:
 
 // Your code here 
-
+age = 42;
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
 // Your code here 
-
+console.log(firstName + lastName + age)
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 const obj = {};
